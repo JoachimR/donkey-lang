70af27bb08acf4efe7ef41296ef06605026fef53
JoachimReiss <reiss@bryter.io>
2023-03-30 22:03:28 +0200

    enable basic CLI usage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+import inquirer from 'inquirer';
+
+async function promptName() {
+  const { name } = await inquirer.prompt({
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  });
+
+  return name;
+}
+
+async function run() {
+  const name = await promptName();
+
+  console.log(`Hello, ${name}!`);
+}
+
+run();
+
